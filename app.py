e07224c1abaf42baa116d25a3bd8e61f8e2438c4
lubowen <1025288028@qq.com>
2024-11-19 19:47:06 +0800

    hello world demo

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-print("Hello World!")
+from fastapi import FastAPI
+from datetime import date
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from fastapi import Request
+import json
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+@app.get("/demo/")
+async def get_demo(a:int = 0, b:int = 0, status_code = 200):
+    sum = a+b
+    data = {"sum": sum, "date": date.today()}
+    return JSONResponse(content=jsonable_encoder(data))
+
+class DemoItem(BaseModel):
+    a: int
+    b: int
+
+@app.post("/demo/")
+async def post_demo(item: DemoItem):
+    print(item)
+    if item.a + item.b == 10:
+        data = {"status": "OK"}
+        return JSONResponse(content=jsonable_encoder(data))
+    data = {"status": "ERROR"}
+    return JSONResponse(content=jsonable_encoder(data))
